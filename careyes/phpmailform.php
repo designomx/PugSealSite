49ad4640055a3d83084ebed31fd353291288d961
Pablo de Jesus Cano Espinoza <trunkszx3@msn.com>
2017-09-04 13:22:47 -0500

    Fase 3 - Fixes + Addons
     Revisar Form de envio de Correo

--- careyes/phpmailform.php
+++ careyes/phpmailform.php
@@ -1,82 +1,28 @@
 <?php
-if(isset($_POST['email'])) {
+error_reporting(0);
+// Guardar los datos recibidos en variables:
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+// Definir el correo de destino:
+$dest = "atencionventas@negociosytarget.com"; 
  
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "tennyson@pugseal.com,dahir@designo.mx";
-    $email_subject = "Contacto Careyes";
+// Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
+$headers = "From: $nombre <$email>\r\n";  
+$headers .= "X-Mailer: PHP5\n";
+$headers .= 'MIME-Version: 1.0' . "\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
  
-    function died($error) {
-        // your error code can go here
-        echo "Por favor revisa tus datos. ";
-        echo "Estos errores se presentaron.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Verifica estos datos por favor.<br /><br />";
-        die();
-    }
+// asunto y armamos el cuerpo del mensaje
+$asunto = "Contacto";
+$cuerpo = "Nombre: ".$nombre."<br>";
+$cuerpo .= "Email: ".$email."<br>";
+$cuerpo .= "Mensaje: ".$mensaje;
  
- 
-    // validation expected data exists
-    if(!isset($_POST['first_name']) ||
-        !isset($_POST['last_name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['telephone']) ||
-        !isset($_POST['comments'])) {
-        died('Lo sentimos, parece que hay algun problema te sugerimos intentar más tarde.');       
-    }
- 
-     
- 
-    $first_name = $_POST['nombre']; // required
-    $email_from = $_POST['email']; // required
-    $comments = $_POST['comments']; // required
- 
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'El email no parece ser valido.<br />';
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'Verifica tu nombre.<br />';
-  }
- 
-  if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
- 
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
- 
-    $email_message = "Form details below.\n\n";
- 
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
- 
-     
- 
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
- 
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
-?>
- 
-<!-- include your own success html here -->
- 
-Thank you for contacting us. We will be in touch with you very soon.
- 
-<?php
- 
+// Esta es una pequena validaciÃ³n, que solo envie el correo si todas las variables tiene algo de contenido:
+if($nombre != '' && $email != '' && $telefono != '' && $mensaje != ''){
+	
+    mail($dest,$asunto,$cuerpo,$headers); //ENVIAR!
+	
 }
 ?>
--- js/main.js
+++ js/main.js
@@ -1,123 +1,188 @@
-!function(a){var b=/iPhone/i,c=/iPod/i,d=/iPad/i,e=/(?=.*\bAndroid\b)(?=.*\bMobile\b)/i,f=/Android/i,g=/(?=.*\bAndroid\b)(?=.*\bSD4930UR\b)/i,h=/(?=.*\bAndroid\b)(?=.*\b(?:KFOT|KFTT|KFJWI|KFJWA|KFSOWI|KFTHWI|KFTHWA|KFAPWI|KFAPWA|KFARWI|KFASWI|KFSAWI|KFSAWA)\b)/i,i=/IEMobile/i,j=/(?=.*\bWindows\b)(?=.*\bARM\b)/i,k=/BlackBerry/i,l=/BB10/i,m=/Opera Mini/i,n=/(CriOS|Chrome)(?=.*\bMobile\b)/i,o=/(?=.*\bFirefox\b)(?=.*\bMobile\b)/i,p=new RegExp("(?:Nexus 7|BNTV250|Kindle Fire|Silk|GT-P1000)","i"),q=function(a,b){return a.test(b)},r=function(a){var r=a||navigator.userAgent,s=r.split("[FBAN");return"undefined"!=typeof s[1]&&(r=s[0]),s=r.split("Twitter"),"undefined"!=typeof s[1]&&(r=s[0]),this.apple={phone:q(b,r),ipod:q(c,r),tablet:!q(b,r)&&q(d,r),device:q(b,r)||q(c,r)||q(d,r)},this.amazon={phone:q(g,r),tablet:!q(g,r)&&q(h,r),device:q(g,r)||q(h,r)},this.android={phone:q(g,r)||q(e,r),tablet:!q(g,r)&&!q(e,r)&&(q(h,r)||q(f,r)),device:q(g,r)||q(h,r)||q(e,r)||q(f,r)},this.windows={phone:q(i,r),tablet:q(j,r),device:q(i,r)||q(j,r)},this.other={blackberry:q(k,r),blackberry10:q(l,r),opera:q(m,r),firefox:q(o,r),chrome:q(n,r),device:q(k,r)||q(l,r)||q(m,r)||q(o,r)||q(n,r)},this.seven_inch=q(p,r),this.any=this.apple.device||this.android.device||this.windows.device||this.other.device||this.seven_inch,this.phone=this.apple.phone||this.android.phone||this.windows.phone,this.tablet=this.apple.tablet||this.android.tablet||this.windows.tablet,"undefined"==typeof window?this:void 0},s=function(){var a=new r;return a.Class=r,a};"undefined"!=typeof module&&module.exports&&"undefined"==typeof window?module.exports=r:"undefined"!=typeof module&&module.exports&&"undefined"!=typeof window?module.exports=s():"function"==typeof define&&define.amd?define("isMobile",[],a.isMobile=s()):a.isMobile=s()}(this);
+! function(a) {
+    var b = /iPhone/i,
+        c = /iPod/i,
+        d = /iPad/i,
+        e = /(?=.*\bAndroid\b)(?=.*\bMobile\b)/i,
+        f = /Android/i,
+        g = /(?=.*\bAndroid\b)(?=.*\bSD4930UR\b)/i,
+        h = /(?=.*\bAndroid\b)(?=.*\b(?:KFOT|KFTT|KFJWI|KFJWA|KFSOWI|KFTHWI|KFTHWA|KFAPWI|KFAPWA|KFARWI|KFASWI|KFSAWI|KFSAWA)\b)/i,
+        i = /IEMobile/i,
+        j = /(?=.*\bWindows\b)(?=.*\bARM\b)/i,
+        k = /BlackBerry/i,
+        l = /BB10/i,
+        m = /Opera Mini/i,
+        n = /(CriOS|Chrome)(?=.*\bMobile\b)/i,
+        o = /(?=.*\bFirefox\b)(?=.*\bMobile\b)/i,
+        p = new RegExp("(?:Nexus 7|BNTV250|Kindle Fire|Silk|GT-P1000)", "i"),
+        q = function(a, b) { return a.test(b) },
+        r = function(a) {
+            var r = a || navigator.userAgent,
+                s = r.split("[FBAN");
+            return "undefined" != typeof s[1] && (r = s[0]), s = r.split("Twitter"), "undefined" != typeof s[1] && (r = s[0]), this.apple = { phone: q(b, r), ipod: q(c, r), tablet: !q(b, r) && q(d, r), device: q(b, r) || q(c, r) || q(d, r) }, this.amazon = { phone: q(g, r), tablet: !q(g, r) && q(h, r), device: q(g, r) || q(h, r) }, this.android = { phone: q(g, r) || q(e, r), tablet: !q(g, r) && !q(e, r) && (q(h, r) || q(f, r)), device: q(g, r) || q(h, r) || q(e, r) || q(f, r) }, this.windows = { phone: q(i, r), tablet: q(j, r), device: q(i, r) || q(j, r) }, this.other = { blackberry: q(k, r), blackberry10: q(l, r), opera: q(m, r), firefox: q(o, r), chrome: q(n, r), device: q(k, r) || q(l, r) || q(m, r) || q(o, r) || q(n, r) }, this.seven_inch = q(p, r), this.any = this.apple.device || this.android.device || this.windows.device || this.other.device || this.seven_inch, this.phone = this.apple.phone || this.android.phone || this.windows.phone, this.tablet = this.apple.tablet || this.android.tablet || this.windows.tablet, "undefined" == typeof window ? this : void 0
+        },
+        s = function() { var a = new r; return a.Class = r, a };
+    "undefined" != typeof module && module.exports && "undefined" == typeof window ? module.exports = r : "undefined" != typeof module && module.exports && "undefined" != typeof window ? module.exports = s() : "function" == typeof define && define.amd ? define("isMobile", [], a.isMobile = s()) : a.isMobile = s()
+}(this);
 
 $(document).ready(function() {
-			$("body").imagesLoaded(function(){
-				$(".page-loader div").fadeOut();
-				$(".page-loader").delay(200).fadeOut("slow");
-			});
 
 
-			 var MOBILE_SITE = 'm/index.html'; // site to redirect to
 
-            // I only want to redirect iPhones, Android phones and a handful of 7" devices
-            if (isMobile.any && $(window).width()<1024) {
 
-                document.location = MOBILE_SITE;
-            }
 
-			$('.hamburger').click(function(){
-				$(this).toggleClass('is-active');
-			});
 
-			$('.parallax-bx').parallax();
 
-//			Casas Animations
+    $("body").imagesLoaded(function() {
+        $(".page-loader div").fadeOut();
+        $(".page-loader").delay(200).fadeOut("slow");
+    });
 
-			$('.belisario-a, .belisario-v').mouseover(function(){
-				$('.belisario-img').toggleClass('animatedx pulse');
-			});
 
-			$('.belisario-a, .belisario-v').mouseleave(function(){
-				$('.belisario-img').toggleClass('animatedx pulse');
-			});
+    var MOBILE_SITE = 'm/index.html'; // site to redirect to
 
-			$('.tennyson-a, .tennyson-v').mouseover(function(){
-				$('.tennyson-img').toggleClass('animatedx pulse');
-			});
+    // I only want to redirect iPhones, Android phones and a handful of 7" devices
+    if (isMobile.any && $(window).width() < 1024) {
 
-			$('.tennyson-a, .tennyson-v').mouseleave(function(){
-				$('.tennyson-img').toggleClass('animatedx pulse');
-			});
+        document.location = MOBILE_SITE;
+    }
 
-			$('.allanpoe-a, .allanpoe-v').mouseover(function(){
-				$('.allanpoe-img').toggleClass('animatedx pulse');
-			});
+    $('.hamburger').click(function() {
+        $(this).toggleClass('is-active');
+    });
 
-			$('.allanpoe-a, .allanpoe-v').mouseleave(function(){
-				$('.allanpoe-img').toggleClass('animatedx pulse');
-			});
+    $('.parallax-bx').parallax();
 
-//			var Wwidth = window.innerWidth;
-			var Wheight = window.innerHeight;
-			var useHam = 0;
+    //			Casas Animations
 
-			$('.hamburger').on('click', function() {
-				 $('.overlay').height(Wheight-100);
+    $('.belisario-a, .belisario-v').mouseover(function() {
+        $('.belisario-img').toggleClass('animatedx pulse');
+    });
 
-				 if (useHam==0) {
-				 	$('.overlay').addClass('open');
-				 	useHam=1;
-				 }else {
-				 	$('.overlay').removeClass('open');
-				 	useHam=0;
-				 }
+    $('.belisario-a, .belisario-v').mouseleave(function() {
+        $('.belisario-img').toggleClass('animatedx pulse');
+    });
 
-			  });
+    $('.tennyson-a, .tennyson-v').mouseover(function() {
+        $('.tennyson-img').toggleClass('animatedx pulse');
+    });
 
-			$('.close-ovrly, .overlay-menu ul a').on('click', function() {
-			  $('.hamburger').toggleClass('is-active');
-			  $('.overlay').removeClass('open');
-			  useHam=0;
-			});
-			
-			var useHamBook = 0;
-			
-			$('.boo-king-a, .show-booking-nav').on('click', function(ev) {
-			
-//				ev.preventDefault();
-				
-				 if (useHam==0) {
-				 	$('.overlay-book').addClass('opened');
-				 	useHamBook=1;
-				 }else {
-				 	$('.overlay-book').removeClass('opened');
-				 	useHamBook=0;
-				 }
+    $('.tennyson-a, .tennyson-v').mouseleave(function() {
+        $('.tennyson-img').toggleClass('animatedx pulse');
+    });
 
-			  });
-			  
-			$('.close-ovrly-book').on('click', function() {
-			  $('.overlay-book').removeClass('opened');
-			  useHamBook=0;
-			});
-			
-			var interSlide = function(){
-			    $('.container-xfade').hide();
-			    $.each( $('.container-xfade'), function(i, este) {
-			        $('div', este).each(function() {
-			            $(this).hide();
-			        });
-			    });
-			    setTimeout(function(){
-			        $('.container-xfade').fadeIn();
-			        var _tiempo=0;
-			        var zindex = 1;
-			        $.each( $('.container-xfade'), function(i, este) {
-			            var _tiempo1=2000;
-			            $('div', este).each(function() {
-			                $(this).css('z-index',zindex).delay(_tiempo).fadeIn(600);
-			                _tiempo+=2000;
-			                zindex++;
-			            });
-			        });
-			        setTimeout(function(){
-			            $('.container-xfade').fadeOut(600);
-			            setTimeout(function(){interSlide()},600);
-			        },_tiempo+600);
-			    },2000);
-			}
-			interSlide();
-			
-			
+    $('.allanpoe-a, .allanpoe-v').mouseover(function() {
+        $('.allanpoe-img').toggleClass('animatedx pulse');
+    });
 
-		});
+    $('.allanpoe-a, .allanpoe-v').mouseleave(function() {
+        $('.allanpoe-img').toggleClass('animatedx pulse');
+    });
+
+    //			var Wwidth = window.innerWidth;
+    var Wheight = window.innerHeight;
+    var useHam = 0;
+
+    $('.hamburger').on('click', function() {
+        $('.overlay').height(Wheight - 100);
+
+        if (useHam == 0) {
+            $('.overlay').addClass('open');
+            useHam = 1;
+        } else {
+            $('.overlay').removeClass('open');
+            useHam = 0;
+        }
+
+    });
+
+    $('.close-ovrly, .overlay-menu ul a').on('click', function() {
+        $('.hamburger').toggleClass('is-active');
+        $('.overlay').removeClass('open');
+        useHam = 0;
+    });
+
+    var useHamBook = 0;
+
+    $('.boo-king-a, .show-booking-nav').on('click', function(ev) {
+
+        //				ev.preventDefault();
+
+        if (useHam == 0) {
+            $('.overlay-book').addClass('opened');
+            useHamBook = 1;
+        } else {
+            $('.overlay-book').removeClass('opened');
+            useHamBook = 0;
+        }
+
+    });
+
+    $('.close-ovrly-book').on('click', function() {
+        $('.overlay-book').removeClass('opened');
+        useHamBook = 0;
+    });
+
+    var interSlide = function() {
+        $('.container-xfade').hide();
+        $.each($('.container-xfade'), function(i, este) {
+            $('div', este).each(function() {
+                $(this).hide();
+            });
+        });
+        setTimeout(function() {
+            $('.container-xfade').fadeIn();
+            var _tiempo = 0;
+            var zindex = 1;
+            $.each($('.container-xfade'), function(i, este) {
+                var _tiempo1 = 2000;
+                $('div', este).each(function() {
+                    $(this).css('z-index', zindex).delay(_tiempo).fadeIn(600);
+                    _tiempo += 2000;
+                    zindex++;
+                });
+            });
+            setTimeout(function() {
+                $('.container-xfade').fadeOut(600);
+                setTimeout(function() { interSlide() }, 600);
+            }, _tiempo + 600);
+        }, 2000);
+    }
+    interSlide();
+
+    /*feruzx ADD contact SEND */
+    $(".boton_envio").click(function() {
+        console.log("entra");
+        var nombre = $("#nombre").val();
+        email = $("#email").val();
+        validacion_email = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+        mensaje = $("#comments").val();
+
+        if (nombre == "") {
+            $("#nombre").focus();
+            $('#nombre').val('Coloca tu nombre!');
+            return false;
+        } else if (email == "" || !validacion_email.test(email)) {
+            $("#email").focus();
+            $('#email').val('Correo No valido!');
+            return false;
+        } else {
+            $('.ajaxgif').removeClass('hide');
+            var datos = 'nombre=' + nombre + '&email=' + email + '&mensaje=' + mensaje;
+            $.ajax({
+                type: "POST",
+                url: "phpmailform.php",
+                data: datos,
+                success: function() {
+                    $('.ajaxgif').hide();
+                    $('.msg').text('Mensaje enviado!').addClass('msg_ok').animate({ 'right': '0px' }, 300);
+                    $(".contacto")[0].reset();
+                },
+                error: function() {
+                    $('.ajaxgif').hide();
+                    $('.msg').text('Hubo un error!').addClass('msg_error').animate({ 'right': '0px' }, 300);
+                }
+            });
+            return false;
+        }
+    });
+});
